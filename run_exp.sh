dbb5e919f31b208bad96d763844650de9ff01d7d
Chris Lin <chris4540@gmail.com>
2019-05-27 22:14:25 +0200

    Update run exp

--- run_exp.sh
+++ run_exp.sh
@@ -2,8 +2,11 @@
 # Setting of the precentage of the labeled data
 export part_labeled=0.3
 
+# train teacher
+python -u train_teacher.py | tee teacher_plbl_${part_labeled}.log
+
 # run semi-supervised learning
 for temp in 1 2 4 8 10; do
     export temp=${temp}
-    python -u semi_main.py | tee log_plbl_${part_labeled}_T${temp}.log
+    python -u train_student.py | tee student_plbl_${part_labeled}_T${temp}.log
 done
