81f8b3f0adbdc6cf8c57d071c5572a8e383cf6d3
Chris Lin <chris4540@gmail.com>
2019-05-27 13:40:25 +0200

    Code refactoring

--- test_lstm_tri.py
+++ test_lstm_tri.py
@@ -1,3 +1,6 @@
+"""
+Baseline model
+"""
 import time
 import torch
 import torch.optim as optim
@@ -9,24 +12,37 @@ import torch.backends.cudnn as cudnn
 from utils import train_one_epoch
 import numpy as np
 
-#
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-if device == 'cuda':
-    cudnn.benchmark = True
+class Config:
+    batch_size = 100
+    init_lr = 0.01  # this would not take effect as using cyclic lr
+    momentum = 0.9
+    weight_decay = 5e-4
+    eta_min = 1e-5
+    eta_max = 1e-2
+    shuffle = True
+    num_hidden_nodes = 78
 
 if __name__ == "__main__":
-    batch_size = 100
-    net = LSTMClassifier(39, 48, n_hidden=78, num_layers=3)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if device == 'cuda':
+        cudnn.benchmark = True
+    net = LSTMClassifier(n_feature=39, n_class=48,
+            n_hidden=Config.num_hidden_nodes, num_layers=3)
+
     traindata = TIMITDataset(root="./data", split="train")
-    trainloader = DataLoader(dataset=traindata, batch_size=batch_size,
-                             shuffle=True, collate_fn=pad_seqs_to_batch)
+    trainloader = DataLoader(dataset=traindata, batch_size=Config.batch_size,
+                             shuffle=Config.shuffle, collate_fn=pad_seqs_to_batch)
     # ==============================
     # optimizer = optim.Adam(net.parameters(), lr=0.05)
-    step_size = 2*np.int(np.floor(len(traindata)/batch_size))
-    print(step_size)
-    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
-    scheduler = optim.lr_scheduler.CyclicLR(optimizer, 1e-5, 1e-2, step_size_up=step_size)
+    step_size = 2*np.int(np.floor(len(traindata)/Config.batch_size))
+
+    optimizer = optim.SGD(
+        net.parameters(), lr=Config.init_lr, momentum=Config.momentum,
+        weight_decay=Config.weight_decay)
+    scheduler = optim.lr_scheduler.CyclicLR(
+        optimizer, Config.eta_min, Config.eta_max, step_size_up=step_size)
     net.to(device)
-    # net.train()
+    # Print out the configuration
+    print("The CyclicLR step size = ", step_size)
     for epoch in range(30):
         train_one_epoch(trainloader, net, optimizer, scheduler=scheduler, device=device)
