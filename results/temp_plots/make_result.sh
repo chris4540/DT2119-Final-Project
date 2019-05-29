2109e21e645b1beb85eafe5a9c89f692c3d91cad
Chris Lin <chris4540@gmail.com>
2019-05-30 00:41:29 +0200

    add make results

--- results/temp_plots/make_result.sh
+++ results/temp_plots/make_result.sh
@@ -9,6 +9,7 @@ root_dir=`readlink -f ../..`
 valid_csv="valid_temp.csv"
 test_csv="test_temp.csv"
 baseline_acc_csv="baseline_acc.csv"
+teacher_acc_csv="teacher_acc.csv"
 
 
 # Create index
@@ -44,15 +45,34 @@ for part_labeled in 0.01 0.03 0.05 0.1 0.2 0.3 0.5; do
     echo $part_labeled >> ${baseline_acc_csv}
 done
 
-echo "valid_acc" > baseline_valid_acc.txt
-echo "test_acc" > baseline_test_acc.txt
+echo "valid_acc" > valid_acc.txt
+echo "test_acc" > test_acc.txt
 for part_labeled in 0.01 0.03 0.05 0.1 0.2 0.3 0.5; do
     file=${root_dir}/experiment/baseline_plbl_${part_labeled}.log
-    tail -n 1 $file | awk '{print $9}' >> baseline_valid_acc.txt
-    tail -n 1 $file | awk '{print $12}' >> baseline_test_acc.txt
+    tail -n 1 $file | awk '{print $9}' >> valid_acc.txt
+    tail -n 1 $file | awk '{print $12}' >> test_acc.txt
 done
-paste -d"," ${baseline_acc_csv} baseline_valid_acc.txt baseline_test_acc.txt > ${baseline_acc_csv}.tmp
+paste -d"," ${baseline_acc_csv} valid_acc.txt test_acc.txt > ${baseline_acc_csv}.tmp
 mv -f ${baseline_acc_csv}.tmp ${baseline_acc_csv}
 
-rm baseline_valid_acc.txt
-rm baseline_test_acc.txt
+rm valid_acc.txt
+rm test_acc.txt
+# ============================================================
+echo "" > ${teacher_acc_csv}
+for part_labeled in 0.01 0.03 0.05 0.1 0.2 0.3 0.5; do
+    echo $part_labeled >> ${teacher_acc_csv}
+done
+
+echo "valid_acc" > valid_acc.txt
+echo "test_acc" > test_acc.txt
+for part_labeled in 0.01 0.03 0.05 0.1 0.2 0.3 0.5; do
+    file=${root_dir}/experiment/teacher_plbl_${part_labeled}.log
+    tail -n 1 $file | awk '{print $9}' >> valid_acc.txt
+    tail -n 1 $file | awk '{print $12}' >> test_acc.txt
+done
+paste -d"," ${teacher_acc_csv} valid_acc.txt test_acc.txt > ${teacher_acc_csv}.tmp
+mv -f ${teacher_acc_csv}.tmp ${teacher_acc_csv}
+
+rm valid_acc.txt
+rm test_acc.txt
+# ============================================================
