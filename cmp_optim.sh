466d5e684235257c8ae2528082a89fc0b7d5f914
Chris Lin <chris4540@gmail.com>
2019-05-29 18:18:12 +0200

    Bug fix

--- cmp_optim.sh
+++ cmp_optim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for part_labeled in 0.1 0.3; do
+    export part_labeled
     for optim in adam stepLR cyclicLR; do
         export optim
         python -u cmp_optim.py |tee cmp_optim_${optim}_plbl_${part_labeled}.log
